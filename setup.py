d599aebb880bdab58197bd25c081f8d7a09e9bbc
Ilyas Kuhlemann <ilyasp.ku@gmail.com>
2021-01-03 18:21:21 +0100

    initial empty repo

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="keep-your-data",
+    version="0.0.1",
+    author="Ilyas Kuhlemann",
+    author_email="ilyasp.ku@gmail.com",
+    description="Keep track of anything, on your own sever.",
+    long_description=long_description,
+    long_description_content_type="text/rst",
+    url=""
